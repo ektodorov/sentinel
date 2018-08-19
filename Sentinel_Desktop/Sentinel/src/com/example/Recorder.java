efad7c2d9fbd40e432f7ef6d16819125c0c8f72c
ektodorov <ek_todorov@abv.bg>
2018-08-19 15:39:58 +0300

    1.Sentinel desktop video recording settings.

--- Sentinel_Desktop/Sentinel/src/com/example/Recorder.java
+++ Sentinel_Desktop/Sentinel/src/com/example/Recorder.java
@@ -1,8 +1,12 @@
 package com.example;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamResolution;
@@ -19,9 +23,11 @@ import com.xuggle.xuggler.video.IConverter;
 public class Recorder {
 
 	private volatile boolean mIsRecording;
+	private SimpleDateFormat mDateFormat;
 	
 	public Recorder() {
 		super();
+		mDateFormat = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss", Locale.getDefault());
 	}
 	
 	public void recordImageJpg(Webcam aWebcam, String aFileName) {
@@ -36,24 +42,34 @@ public class Recorder {
 	}
 	
 	public void recordStart(Webcam aWebcam, String aFileName) {
-		File file = new File(aFileName + ".ts");
+		File file = new File(aFileName + ".mpeg");
 		int unique = 2;
 		while(file.exists()) {
 			aFileName = aFileName + "_" + unique;
 			unique++;
-			file = new File(aFileName + ".ts");
+			file = new File(aFileName + ".mpeg");
 		}
 		
 		IMediaWriter writer = ToolFactory.makeWriter(file.getName());
 		Dimension size = WebcamResolution.VGA.getSize();
-		writer.addVideoStream(0, 0, ICodec.ID.CODEC_ID_H264, size.width, size.height);
+		//writer.addVideoStream(0, 0, ICodec.ID.CODEC_ID_H264, size.width, size.height);
+		//writer.addVideoStream(0, 0, ICodec.ID.CODEC_ID_MPEG1VIDEO, size.width, size.height);
+		writer.addVideoStream(0, 0, ICodec.ID.CODEC_ID_MPEG2VIDEO, size.width, size.height);
 
 		long start = System.currentTimeMillis();
 		mIsRecording = true;
 		int keyFrame = 0;
+		Date date = null;
+		String time = null;
 		while(mIsRecording) {	
 			BufferedImage image = ConverterFactory.convertToType(aWebcam.getImage(), BufferedImage.TYPE_3BYTE_BGR);
 			IConverter converter = ConverterFactory.createConverter(image, IPixelFormat.Type.YUV420P);
+			
+			date = new Date();
+			time = mDateFormat.format(date);
+			Graphics graphics = image.getGraphics();
+			graphics.drawString(time, 20, 30);
+			graphics.dispose();
 			
 			IVideoPicture frame = converter.toPicture(image, (System.currentTimeMillis() - start) * 1000);
 			frame.setKeyFrame(keyFrame == 0);
@@ -61,13 +77,16 @@ public class Recorder {
 			
 			writer.encodeVideo(0, frame);
 			
-			//20 FPS
+			//25 FPS
 			try {
-				Thread.sleep(50);
+				Thread.sleep(40);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 			keyFrame++;
+			if(keyFrame > 1) {
+				keyFrame = 0;
+			}
 		}
 		writer.close();
 	}
